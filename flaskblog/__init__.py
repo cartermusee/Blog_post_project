88a6e53d7def7ec3f9fc766a19db6d3704c13d0d
macilah-carter <museemacilah@gmail.com>
2024-03-27 11:43:03 +0300

    added the db in the varibles

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///site.db' 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
